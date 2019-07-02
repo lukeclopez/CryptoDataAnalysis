de50499b3c7cecf4a107ffbd3dfa67d446ed46fd
Luke Lopez <xlucas14@gmail.com>
2019-07-02 11:34:52 -0400

    Object oriented approach

--- report_gen.py
+++ report_gen.py
@@ -9,38 +9,66 @@ path = "message_service_message_y2018d311.csv"
 
 df = pd.read_csv(path)
 
+channel_days = []
+
+class ChannelDay:
+    def __init__(self, id):
+        channel_days.append(self)
+        self.id = id
+        self.unique_posters = 0
+        self.total_messages = 0
+        self.est_percentage_of_questions = 0.0
+        self.top_posters_pie_chart = {}
+
+    def __str__(self):
+        return f"Channel {self.id}"
+
 # Find number of unique posters in each channel
 channel_ids = df["channel_id"].values
 channel_ids_unique = set(channel_ids)
 
-user_ids_unique = 5
 
 ## For each day and channel id, find the number of unique posters
 for channel_id in channel_ids_unique:
+    new_channel_day = ChannelDay(channel_id)
+
     channel_rows = df.loc[df["channel_id"] == channel_id]
+    
     user_ids = channel_rows["user_id"].values
     user_ids_unique = set(user_ids)
+    
     total_unique_users = len(user_ids_unique)
-    print(f"Channel: {channel_id} Unique Posters: {total_unique_users}")
+    new_channel_day.unique_posters = total_unique_users
+
+    # Find the number of messages per channel per day
+    new_channel_day.total_messages = len(channel_rows)
+
+    # Find estimated percentage of questions
+    question_rows = channel_rows.loc[channel_rows["message_text"].str.contains("\?")]
+    no_of_questions = len(question_rows)
+    new_channel_day.est_percentage_of_questions = no_of_questions / new_channel_day.total_messages
+
+    # Make a "pie chart" of the top 10 posters
+    user_message_percents = {}
+    for uuid in user_ids_unique:
+        user_rows = channel_rows.loc[channel_rows["user_id"] == uuid]
+
+        # The AMOUNT of messages is the key, the user ID is the value.
+        # I did it this way to make it easier to match amounts 
+        user_message_percents[uuid] = len(user_rows) / len(channel_rows)
+
+    new_channel_day.top_posters_pie_chart = user_message_percents
+
+    break
+
+    
+
+
 
 
 
 # Find how many people post daily
 ## Same as number 1?
-
-# Find the number of messages per channel per day
-no_of_messages = len(df)
-
-
-# Find estimated percentage of questions
-question_rows = df.loc[df["message_text"].str.contains("\?")]
-no_of_questions = len(question_rows)
-est_percentage_of_questions = no_of_questions / no_of_messages
-
-
-# Get the top 10 posters (across days) and find what percentage of messages they are responsible for.
-## Get the top 10 for each day, then find the names (or ids) that appear most frequently in 7 and 30-day periods
-
 
 # df.to_csv("info/columns.csv", encoding="utf-8")
 
